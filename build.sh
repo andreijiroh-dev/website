089aefa5a92567907e262e0781ca5c0ace5e43e6
Andrei Jiroh Halili <ajhalili2006@gmail.com>
2023-02-18 17:50:05 +0000

    chore(global): update redirects and config again (including build.yml one)
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 set -xe
 
 TARGET_DIR=${TARGET_DIR:-"$HOME/public_html"}
+FF_OFFLINE_MKDOCS_PLUGIN=${FF_OFFLINE_MKDOCS_PLUGIN:-"false"}
 
-if ! command -v mkdocs > /dev/null; then
-  pip3 install -r requirements.txt --user
-fi
-
+pip3 install -r requirements.txt --user
 mkdocs build -d $TARGET_DIR
 
 set +xe
