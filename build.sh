eab9ae94a66d22acf1df36ef0856dacde4638eb2
Andrei Jiroh Halili <ajhalili2006@gmail.com>
2023-03-27 15:29:38 +0000

    fix(gitlab-ci): :green_heart: fix issue relating to container entrypoint stuff
    Also in this commit, we improved the build script to
    use the virtualenv setup we did to isolate dependencies from the rest
    of the build system. We also did
    some adjustments on caching deps to speed up the CI.
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -xe
 
-TARGET_DIR=${TARGET_DIR:-"$HOME/public_html"}
+TARGET_DIR=${TARGET_DIR:-"$PWD/public"}
 FF_OFFLINE_MKDOCS_PLUGIN=${FF_OFFLINE_MKDOCS_PLUGIN:-"false"}
+FF_ENABLE_COMMIT_DATA=${FF_ENABLE_COMMIT_DATA:-"true"}
+FF_GENERATE_SOCIAL_CARDS=${FF_GENERATE_SOCIAL_CARDS:-"true"}
 
-pip3 install -r requirements.txt --user
-mkdocs build -d $TARGET_DIR
+if [[ ! -d "$PWD/.venv" ]]; then
+  python3 -m venv $PWD/.venv
+fi
+$PWD/.venv/bin/pip3 install -r requirements.txt --upgrade
+$PWD/.venv/bin/mkdocs build -d $TARGET_DIR
 
 set +xe
