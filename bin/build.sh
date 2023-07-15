1a3b255aafb8319e5052f5269621f056727bb3b8
Andrei Jiroh Halili <ajhalili2006@gmail.com>
2023-07-15 18:02:41 +0000

    Improve feature flag detection on builds
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -8,9 +8,13 @@ FF_GENERATE_SOCIAL_CARDS=${FF_GENERATE_SOCIAL_CARDS:-"true"}
 
 if [[ ! -d "$PWD/.venv" && $SKIP_VENV_SETUP == "" ]]; then
   python3 -m venv $PWD/.venv
+  $PWD/.venv/bin/pip3 install -r requirements.txt --upgrade
+  $PWD/.venv/bin/mkdocs build -d $TARGET_DIR
+else
+  pip3 install -r requirements.txt --upgrade
+  mkdocs build -d $TARGET_DIR
 fi
-$PWD/.venv/bin/pip3 install -r requirements.txt --upgrade
-$PWD/.venv/bin/mkdocs build -d $TARGET_DIR
+
 mkdir "$TARGET_DIR/api"
 git rev-parse HEAD > "$TARGET_DIR/api/commit"
 
