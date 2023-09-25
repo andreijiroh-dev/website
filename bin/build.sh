0bf37f8c53551a90ec6616683aeb7dc4280070c3
Andrei Jiroh Halili <ajhalili2006@gmail.com>
2023-09-25 14:32:29 +0000

    ci(global): update CI image tags to use latest commit builds
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@gmail.com>

--- bin/build.sh
+++ bin/build.sh
@@ -7,9 +7,8 @@ FF_ENABLE_COMMIT_DATA=${FF_ENABLE_COMMIT_DATA:-"true"}
 FF_GENERATE_SOCIAL_CARDS=${FF_GENERATE_SOCIAL_CARDS:-"true"}
 
 if [[ ! -d "$PWD/.venv" && $SKIP_VENV_SETUP == "" ]]; then
-  python3 -m venv $PWD/.venv
-  $PWD/.venv/bin/pip3 install -r requirements.txt --upgrade
-  $PWD/.venv/bin/mkdocs build -d "$TARGET_DIR"
+  pipenv install -r requirements.txt
+  pipenv run -- mkdocs build -d "$TARGET_DIR"
 else
   pip3 install -r requirements.txt --upgrade
   mkdocs build -d "$TARGET_DIR"
