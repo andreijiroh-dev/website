8362b6390e036ac143317b29df6a99aad3fe22ba
Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>
2023-10-07 01:03:54 +0800

    ci(build): forget to include changes for build script itself
    This should be originally on 45518990974e241e536e498fdbd3d7a1360956bf,
    but we don't want to do a force push due to reasons.
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.eu.org>

--- bin/build.sh
+++ bin/build.sh
@@ -6,16 +6,17 @@ FF_OFFLINE_MKDOCS_PLUGIN=${FF_OFFLINE_MKDOCS_PLUGIN:-"false"}
 FF_ENABLE_COMMIT_DATA=${FF_ENABLE_COMMIT_DATA:-"true"}
 FF_GENERATE_SOCIAL_CARDS=${FF_GENERATE_SOCIAL_CARDS:-"true"}
 
-if [[ $SKIP_VENV_SETUP == "" ]] || [[ $CI == "" ; then
+if [[ $SKIP_VENV_SETUP == "" ]] || [[ $CI == "" ]]; then
   pipenv install -r requirements.txt
   pipenv run -- mkdocs build -d "$TARGET_DIR"
 else
-  pip3 install -r requirements.txt --upgrade
+  pip3 install -r requirements.txt --upgrade --user
   mkdocs build -d "$TARGET_DIR"
 fi
 
 mkdir "$TARGET_DIR/api"
 git rev-parse HEAD > "$TARGET_DIR/api/commit"
 cp "$TARGET_DIR/assets/images/favicon.png" "$TARGET_DIR/favicon.ico"
+cp markdown/.well-known "$TARGET_DIR/.well-known" -rv
 
 set +xe
