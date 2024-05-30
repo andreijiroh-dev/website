95d62c800bedf7250f8e85824cc2d04d2e44330a
Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>
2024-05-30 15:22:15 +0800

    build(scripts): include humans.txt in post-build step
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>

--- bin/build.sh
+++ bin/build.sh
@@ -13,7 +13,7 @@ fi
 
 if [[ $SKIP_VENV_SETUP == "" ]] || [[ $CI == "" ]]; then
   pipenv install --ignore-pipfile --deploy --verbose
-  pipenv run -- mkdocs build -d "$TARGET_DIR" -f ${TARGET_BUILD_CONFIG}
+  pipenv run mkdocs build -d "$TARGET_DIR" -f ${TARGET_BUILD_CONFIG}
 else
   pip3 install -r requirements.txt --upgrade --user
   mkdocs build -d "$TARGET_DIR" -f ${TARGET_BUILD_CONFIG}
@@ -23,5 +23,6 @@ mkdir "$TARGET_DIR/api"
 git rev-parse HEAD > "$TARGET_DIR/api/commit"
 cp "$TARGET_DIR/assets/images/favicon.png" "$TARGET_DIR/favicon.ico"
 cp markdown/.well-known "$TARGET_DIR/.well-known" -rv
+cp markdown/humans.txt "$TARGET_DIR/humans.txt" -v
 
 set +xe
