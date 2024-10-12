25d3523d96ce4db6451bdcae6e94c9aed89e7e2b
Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>
2024-10-12 19:28:53 +0000

    chore(global): update configs and scripts
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.xyz>

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-set -e
+set -ex
 
 TARGET_DIR="${PWD}/public"
 pipenv run build
 cp "$TARGET_DIR/assets/images/favicon.png" "$TARGET_DIR/favicon.ico"
-cp markdown/.well-known markdown/humans.txt "$TARGET_DIR/" -rv
