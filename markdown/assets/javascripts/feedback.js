302580ca9c30e73a73c2b14475341daa454b1044
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-10-14 05:20:46 +0800

    chore(mkdocs): setup PostHog + Cloudflare Web Analytics integration
    Also updated the rest of configurations behind the scenes
    
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

---
+++ markdown/assets/javascripts/feedback.js
@@ -0,0 +1,23 @@
+document$.subscribe(function() {
+  var feedback = document.forms.feedback
+  if (typeof feedback === "undefined") return
+
+  feedback.hidden = false
+
+  feedback.addEventListener("submit", function(ev) {
+    ev.preventDefault()
+
+    var page = document.location.pathname
+    var data = ev.submitter.getAttribute("data-md-value")
+
+    posthog.capture("page_feedback", { page, data })
+
+    feedback.firstElementChild.disabled = true
+
+    var note = feedback.querySelector(
+      ".md-feedback__note [data-md-value='" + data + "']"
+    )
+    if (note)
+      note.hidden = false
+  })
+}
