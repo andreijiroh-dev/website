9298a0d2f41b6ace6d7a7f589eb24190eea1cff8
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-12-08 08:57:13 +0000

    chore: modify build script to allow skipping `pipenv run build`
    Signed-off-by: Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>

--- bin/build.sh
+++ bin/build.sh
@@ -5,6 +5,11 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 SOURCE_DIR="${GIT_ROOT}/markdown"
 TARGET_DIR="${GIT_ROOT}/public"
 
-pipenv run build
+if [[ $SKIP_BUILD == "" ]]; then
+  pipenv run build
+else
+  echo "Skipping build step"
+fi
+
 cp "$TARGET_DIR/assets/images/favicon.png" "$TARGET_DIR/favicon.ico" -v
 cp "$SOURCE_DIR/.well-known" "$TARGET_DIR/" -rv
